710b71edd3cdc8b9f0bf7d13b269d22af7c5fa61
kirito22037 <kirito22037@gmail.com>
2020-10-05 23:14:09 +0530

    remove body-parser package , and implement express bodyparser

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,5 @@
 const express=require('express');
 const app=express();
-const bodyparser=require('body-parser');
 const connection=require('./connection');
 const Post=require('./routes/route');
 const cors = require('cors');
@@ -12,8 +11,8 @@ path=require('path');
 app.use(fileupload());
 app.set('views', __dirname + '/views');
 app.use(cors());
-app.use(bodyparser.urlencoded({ extended: true })); 
-app.use(bodyparser.json());
+app.use(express.urlencoded()); 
+app.use(express.json());
 app.use('/post',Post);
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
